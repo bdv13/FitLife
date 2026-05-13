a210d53d6ee37bb748e4f9844ea54b534017bdb3
Denis Baidikov <baydikovdenis@gmail.com>
2026-05-13 18:14:04 +0200

    First commit.

--- fit_life.py
+++ fit_life.py
@@ -1,26 +1,68 @@
-# Проект FitLife - MVP версия 1.0
+import time
 
 
-# 1. Знакомство
-# TODO: Спроси у пользователя имя и сохрани в переменную user_name
-# TODO: Спроси возраст и сохрани в переменную user_age (не забудь преобразовать в число)
+WATER_PER_KG_ML = 30
+ML_IN_LITER = 1000
 
 
-# 2. Сбор данных
-# TODO: Запроси вес (в кг) и сохрани в user_weight (тип float)
-# TODO: Запроси рост (в метрах, например 1.75) и сохрани в user_height (тип float)
+def calc_bmi(user_weight, user_height):
+    """Calculate body mass index (BMI)."""
+    return user_weight / (user_height ** 2)
 
 
-# 3. Логика расчетов (Функции как "черный ящик": используем арифметику)
-# Формула ИМТ: вес разделить на (рост в квадрате)
-# TODO: Рассчитай bmi (Индекс массы тела)
+def calc_water_needed(user_weight):
+    """Calculate recommended daily water intake (in liters)."""
+    return user_weight * WATER_PER_KG_ML / ML_IN_LITER
 
 
-# Подсчет воды: вес * 30 мл
-# TODO: Рассчитай water_needed
+def display_results(user_name, user_age, bmi, water_needed):
+    """Display the results to the user"""
+    print(
+        f'{user_name}, thank you for waiting!\n'
+        f'Your age: {user_age} years\n'
+        f'Your body mass index (BMI): {bmi:.1f}\n'
+        f'Recommended daily water intake: {water_needed:.2f} l\n'
+        f'Calculation complete. Stay healthy!'
+    )
 
 
-# 4. Вывод красивого результата
-# TODO: Используй f-строку, чтобы вывести приветствие, например: "Привет, Иван!"
-# TODO: Выведи возраст, ИМТ (округленный до 1 знака) и норму воды.
-print("Расчет окончен. Будьте здоровы!")
+def main():
+    """Main function to run the FitLife application."""
+    print('Welcome to FitLife!', end='\n\n')
+
+    user_name = input(
+        'Hi, what is your name? ')
+
+    while True:
+        try:
+            user_age = int(input('Please enter your age: '))
+            break
+        except ValueError:
+            print('Please enter a valid age (number).')
+
+    while True:
+        try:
+            user_weight = float(input('Please enter your weight in kg: '))
+            break
+        except ValueError:
+            print('Please enter a valid weight (e.g., 70.5).')
+
+    while True:
+        try:
+            user_height = float(input('Please enter your height in meters: '))
+            break
+        except ValueError:
+            print('Please enter a valid height (e.g., 1.75).')
+
+    bmi = calc_bmi(user_weight, user_height)
+    water_needed = calc_water_needed(user_weight)
+
+    print()
+    print('Thank you, your data has been saved. '
+          'Calculating BMI and water intake...', end='\n\n')
+    time.sleep(0.2)
+    display_results(user_name, user_age, bmi, water_needed)
+
+
+if __name__ == '__main__':
+    main()
